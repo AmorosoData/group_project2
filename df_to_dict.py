95422e20d5559283f94717a4777c140bdd6ed3eb
Cullen Andrews <66756039+cullen-andrews@users.noreply.github.com>
2020-10-28 19:13:10 -0700

    changes to app.py, df_to_dict

--- df_to_dict.py
+++ df_to_dict.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 def make_dict():
 
-    data_df = pd.read_csv('./data/cleaned_nuke_csv')
+    data_df = pd.read_csv('./data/nuke_coordinates.csv')
     del data_df["Unnamed: 0"]
-    data_df = data_df[["Country_Year", "Country", "Code", "Year", "Quantity of Nuclear Weapons", "GDP (Current LCU)"]]
+    data_df = data_df[["Country_Year", "Country", "Code", "Year", "Quantity_of_Nuclear_Weapons", "GDP (Current LCU)", "Latitude", "Longitude"]]
     data_df = data_df.set_index("Country_Year")
     
     final_dict = data_df.to_dict("index")
