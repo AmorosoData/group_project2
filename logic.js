1f9755635118936fb69431dea8d77e806b417e8d
Pleon <patrickedwardleon@gmail.com>
2020-10-29 14:21:16 -0700

    Work on index.html and logic.js

--- logic.js
+++ logic.js
@@ -13,7 +13,62 @@ var url = `data/coordinates_json.json`;
 
 console.log(url);
 
-d3.json(url).then(function(data) {
-    console.log(data);
-});
+function grabData(data) {
 
+    d3.json(url).then(function(d) {
+      var country_year = d.Country_Year;
+      var arsenal = d.Quantity_of_Nuclear_Weapons;
+      var gdp = d.GDP;
+      var lat = d.Latitude;
+      var long = d.Longitude;
+      var country = d.Country;
+      var year = d.Year;
+
+      console.log(country_year);
+      console.log(arsenal);
+      console.log(gdp);
+      console.log(lat);
+      console.log(long);
+      console.log(country);
+      console.log(year);
+
+  function renderCharts() {
+      var trace1 = {
+        type: "scatter",
+        mode: "lines",
+        name: "Nuclear Arsenal vs GDP",
+        x: arsenal,
+        y: gdp,
+        line: {
+          color: "#17BECF"
+        }
+      };
+  
+      var data = [trace1];
+  
+      var layout = {
+        // Need to instatiate input tag on html for user input on user_country_year
+        title: "Arsenal vs GDP",
+        xaxis: {
+          range: [0, Math.max(arsenal)],
+          type: "linear"
+        },
+        yaxis: {
+          autorange: true,
+          type: "linear"
+        }
+      };
+  
+      Plotly.newPlot("plot", data);
+    }
+    renderCharts();
+  });
+}
+grabData();
+
+
+
+// Scatterplot: arsenal vs gdp
+
+// Heatmap: Density of Nukes by country
+// Transitional: suicide rate vs arsenal, happiness vs arsenal
