f48787d58ef399d4d752c900d9a304c45f19e703
Kelly Shull <kelly.t.shull@gmail.com>
2020-11-02 13:39:53 -0800

    index html changes

--- static/js/app.js
+++ static/js/app.js
@@ -3,47 +3,47 @@
 // var url = `data/index_coordinates.json`;
 
 
-var dropDown = d3.select("#selDataset");
-var jsonData;
+// var dropDown = d3.select("#selDataset");
+// var jsonData;
 
 
-// will replace url with app route path
-d3.json('/country').then(function (data) {
-    jsonData = data;
-    // var names = jsonData.names;
-    var names = jsonData.Country_Year;
+// // will replace url with app route path
+// d3.json('/country').then(function (data) {
+//     jsonData = data;
+//     // var names = jsonData.names;
+//     var names = jsonData.Country_Year;
 
 
-    names.map(d => {
-        dropDown.append("option")
-            .property('value', d)
-            .text(d)
-    });
-    optionChanged(names[0]);
+//     names.map(d => {
+//         dropDown.append("option")
+//             .property('value', d)
+//             .text(d)
+//     });
+//     optionChanged(names[0]);
 
-});
+// });
 
-function optionChanged(newName) {
-    // console.log(newName);
-    metaData(newName)
-}
+// function optionChanged(newName) {
+//     // console.log(newName);
+//     metaData(newName)
+// }
 
-function metaData(Country_Year) {
-    console.log(`Selection: ${Country_Year}`)
-    // var nukeCountryData = jsonData.data.filter(item => item.Country_Year == Country_Year)[0];
-    var nukeCountryData = jsonData.filter(item => item.Country_Year == Country_Year)[0];
-    var sampleMetadata = d3.select("#sample-metadata");
-    // reset the html
-    sampleMetadata.html("");
+// function metaData(Country_Year) {
+//     console.log(`Selection: ${Country_Year}`)
+//     // var nukeCountryData = jsonData.data.filter(item => item.Country_Year == Country_Year)[0];
+//     var nukeCountryData = jsonData.filter(item => item.Country_Year == Country_Year)[0];
+//     var sampleMetadata = d3.select("#sample-metadata");
+//     // reset the html
+//     sampleMetadata.html("");
 
 
-    for (const [key, value] of Object.entries(nukeCountryData)) {
-        console.log(`${key}: ${value}`);
-        sampleMetadata.append('p')
-            .text(`${key}: ${value}`)
+//     for (const [key, value] of Object.entries(nukeCountryData)) {
+//         console.log(`${key}: ${value}`);
+//         sampleMetadata.append('p')
+//             .text(`${key}: ${value}`)
 
-    }
-}
+//     }
+// }
 
 // will replace countries with path to app route
 // var countries = `data/index_coordinates.json`;
